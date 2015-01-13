9b27d23e8e3ac50f997bce9675d15126134b552e
Martind64 <Martind_64@hotmail.com>
2015-01-13 15:12:07 +0100

    Some of the syntaxes

---
+++ LinqAssignment/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Teacher> Teachers = new List<Teacher>();
+            Teachers.Add(new Teacher("Ebbe", 1, 24000));
+            Teachers.Add(new Teacher("Soeren", 2, 22000));
+            Teachers.Add(new Teacher("Peter", 3, 23000));
+            Teachers.Add(new Teacher("Lars", 4, 30000));
+
+
+             //Query Syntax
+            //var QuerySyntaxAll = from t in Teachers select t._name;
+
+            //foreach (var teacher in QuerySyntaxAll)
+            //{
+            //    Console.WriteLine(teacher);
+            //}
+
+
+            //Expression Syntax
+            //var ExpressionSyntaxAll = Teachers.Select(teach => teach._name);
+            //foreach (var teacher in ExpressionSyntaxAll)
+            //{
+            //    Console.WriteLine(teacher);
+            //}
+
+
+            // Query Syntax
+            //var QuerySyntaxHighSalary = from t in Teachers where t._salary > 23000 select t._name;
+            //foreach (var teacher in QuerySyntaxHighSalary)
+            //{
+            //    Console.WriteLine(teacher);
+            //}
+
+            //Expression Syntax
+            //var ExpressionSyntaxHighSalary = Teachers.Where(teach => teach._salary > 23000);
+
+            //foreach (var teacher in ExpressionSyntaxHighSalary)
+            //{
+            //    Console.WriteLine(teacher._name);
+            //}
+
+
+            //Query Syntax
+            //var QuerySyntaxOrderedNames = from t in Teachers orderby t._name select t._name;
+            //foreach (var teacher in QuerySyntaxOrderedNames)
+            //{
+            //    Console.WriteLine(teacher);
+            //}
+
+
+            //Expression Syntax
+            //var ExpressionSyntaxOrderedByNames = Teachers.OrderBy(teach => teach._name);
+
+            //foreach (var teacher in ExpressionSyntaxOrderedByNames)
+            //{
+            //    Console.WriteLine(teacher._name);
+            //}
+
+        }
+    }
+}
---
+++ LinqAssignment/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAssignment
+{
+    public class Teacher
+    {
+        public string _name { get; set; }
+        public int _id { get; set; }
+        public int _salary { get; set; }
+
+        public Teacher(string name, int id, int salary)
+        {
+            _name = name;
+            _id = id;
+            _salary = salary;
+        }
+
+    }
+}
